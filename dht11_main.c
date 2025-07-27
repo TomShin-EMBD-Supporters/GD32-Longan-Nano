781c08f9689af96c484760ecd48049f674182eea
TomShin <sjaehun484@gmail.com>
2025-07-27 13:46:10 +0900

    Update dht11_main.c

--- dht11_main.c
+++ dht11_main.c
@@ -4,14 +4,15 @@
 #include "DHT11.h"
 #include <stdio.h>
 
+ 
+uint8_t values[5];
 uint8_t text[20];
-
+ 
 void main(void)
 {
     uint8_t state;
     float temperature = 0.0;
     float humidity = 0.0;
-    uint8_t values[5];
 
     SystemInit();
     SystemCoreClockUpdate();
@@ -22,23 +23,30 @@ void main(void)
 
     while (1)
     {
-        state = DHT11_get_data(values);  
+        state = DHT11_get_data(); 
 
         OLED_clear_buffer();
 
         if (state == 0)   
         {
-            temperature = values[2] + values[3] * 0.1 ;
+            temperature = values[2] + values[3] * 0.1;
             humidity = values[0] + values[1] * 0.1;
-    
-           sprintf((char*)text, "Temp: %.2f C", temperature);
-          OLED_print_string(0, 2, text);
 
-          sprintf((char*)text, "Humid: %.2f %%", humidity);
-          OLED_print_string(0, 4, text);
+            sprintf((char*)text, "Temp: %.2f C", temperature);
+            OLED_print_string(0, 2, text);
 
+            sprintf((char*)text, "Humid: %.2f %%", humidity);
+            OLED_print_string(0, 4, text);
         }
-       
+        else if (state == 1)
+        {
+            OLED_print_string(0, 2, "                      "); //No Sensor Response
+        }
+        else if (state == 2)
+        {
+            OLED_print_string(0, 2, "Checksum error");
+        }
+        
         delay_1ms(1000);
     }
 }
